6a3f18cedc6454e47cd55b83000f3b0195122d3c
wangxingang <wangxingang0610@sina.com>
2023-03-10 23:02:16 +0800

    eureka - 非集群

--- springcloud-provider-payment8001/src/main/java/com/wangxg/springcloud/PaymentMain8001.java
+++ springcloud-provider-payment8001/src/main/java/com/wangxg/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.wangxg.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @auther zzyy
  * @create 2020-02-17 21:13
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001
 {
     public static void main(String[] args) {
