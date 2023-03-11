855492525d55aaa17c26cc3a73e1f2240ad4360c
wangxingang <wangxingang0610@sina.com>
2023-03-11 19:31:51 +0800

    eureka - 集群

--- springcloud-consumer-order80/src/main/java/com/wangxg/springcloud/controller/OrderController.java
+++ springcloud-consumer-order80/src/main/java/com/wangxg/springcloud/controller/OrderController.java
@@ -13,7 +13,11 @@ import javax.annotation.Resource;
 @RestController
 public class OrderController {
 
-    public static final String PARMENT_URL = "http://localhost:8001";
+//    public static final String PARMENT_URL = "http://localhost:8001";
+
+    // 通过在eureka上注册过的微服务名称调用
+    public static final String PARMENT_URL = "http://SPRINGCLOUD-PAYMENT-SERVICE";
+
     @Resource
     private RestTemplate restTemplate;
 
--- springcloud-provider-payment8002/src/main/java/com/wangxg/springcloud/controller/PaymentController.java
+++ springcloud-provider-payment8002/src/main/java/com/wangxg/springcloud/controller/PaymentController.java
@@ -2,10 +2,10 @@ package com.wangxg.springcloud.controller;
 
 import com.wangxg.springcloud.entities.CommonResult;
 import com.wangxg.springcloud.entities.Payment;
-import com.wangxg.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import com.wangxg.springcloud.service.PaymentService;
 
 import javax.annotation.Resource;
 
@@ -15,32 +15,27 @@ import javax.annotation.Resource;
  */
 @RestController
 @Slf4j
-public class PaymentController
-{
+public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
-
     @Resource
     private PaymentService paymentService;
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(@RequestBody Payment payment)
-    {
+    public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
-        log.info("*****插入结果："+result);
+        log.info("*****插入结果：" + result);
 
-        if(result > 0)
-        {
-            return new CommonResult(200,"插入数据库成功,serverPort: " + serverPort);
-        }else{
-            return new CommonResult(444,"插入数据库失败",null);
+        if (result > 0) {
+            return new CommonResult(200, "插入数据库成功,serverPort: " + serverPort);
+        } else {
+            return new CommonResult(444, "插入数据库失败", null);
         }
     }
 
     @GetMapping(value = "/payment/get/{id}")
-    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id)
-    {
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
         Payment payment = paymentService.getPaymentById(id);
         log.info("*****查询结果:{}", payment);
         if (payment != null) {
