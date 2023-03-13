487a78a043152ee121cc675ab988711c01d56fca
wangxingang <wangxingang0610@sina.com>
2023-03-13 18:03:43 +0800

    hystrix 提供者 - 降级处理

--- springcloud-provider-hystrix-payment8001/src/main/java/com/wangxg/springcloud/PaymentHystrixMain8001.java
+++ springcloud-provider-hystrix-payment8001/src/main/java/com/wangxg/springcloud/PaymentHystrixMain8001.java
@@ -2,10 +2,12 @@ package com.wangxg.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient //本服务启动后会自动注册进eureka服务中
+@EnableCircuitBreaker
 public class PaymentHystrixMain8001
 {
     public static void main(String[] args)
--- springcloud-provider-hystrix-payment8001/src/main/java/com/wangxg/springcloud/service/PaymentService.java
+++ springcloud-provider-hystrix-payment8001/src/main/java/com/wangxg/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.wangxg.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -22,9 +24,17 @@ public class PaymentService
      * @param id
      * @return
      */
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="3000")
+    })
     public String paymentInfo_TimeOut(Integer id)
     {
-        try { TimeUnit.SECONDS.sleep(3); } catch (InterruptedException e) { e.printStackTrace(); }
+        try { TimeUnit.SECONDS.sleep(5); } catch (InterruptedException e) { e.printStackTrace(); }
         return "线程池:"+Thread.currentThread().getName()+"paymentInfo_TimeOut,id: "+id+"\t"+"O(∩_∩)O，耗费3秒";
+    }
+
+
+    public String paymentInfo_TimeOutHandler(Integer id){
+        return "/(ㄒoㄒ)/调用支付接口超时或异常：\t"+ "\t当前线程池名字" + Thread.currentThread().getName();
     }
 }
