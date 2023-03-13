1b2035f506f097d4fa5d9c4aea17b6b730369aad
wangxingang <wangxingang0610@sina.com>
2023-03-13 18:09:34 +0800

    hystrix 消费者 - 降级处理

--- springcloud-consumer-feign-hystrix-order80/src/main/java/com/wangxg/springcloud/controller/OrderHystirxController.java
+++ springcloud-consumer-feign-hystrix-order80/src/main/java/com/wangxg/springcloud/controller/OrderHystirxController.java
@@ -1,5 +1,7 @@
 package com.wangxg.springcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.wangxg.springcloud.client.PaymentFeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,8 +20,17 @@ public class OrderHystirxController {
         return paymentFeignClient.paymentInfo_OK(id);
     }
 
+
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="1500")
+    })
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
         return paymentFeignClient.paymentInfo_TimeOut(id);
+    }
+
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id)
+    {
+        return "我是消费者80,对方支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,o(╥﹏╥)o";
     }
 }
