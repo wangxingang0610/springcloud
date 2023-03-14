c0fd8f5477908b7a345a11db72eb0effba151d47
wangxingang <wangxingang0610@sina.com>
2023-03-14 14:28:17 +0800

    hystrix 服务熔断

---
+++ springcloud-consumer-hystrix-dashboard9001/src/main/java/com/wangxg/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,11 @@
+package com.wangxg.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
