7307d8ab03d53b7ac4447c0330d8a73afb2ea22c
wangxingang <wangxingang0610@sina.com>
2023-03-18 16:41:14 +0800

    stream

---
+++ springcloud-stream-rabbitmq-provider8801/src/main/java/com/wangxg/springcloud/controller/SendMessageController.java
@@ -0,0 +1,21 @@
+package com.wangxg.springcloud.controller;
+
+import com.wangxg.springcloud.service.IMessageProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class SendMessageController
+{
+    @Resource
+    private IMessageProvider messageProvider;
+
+    @GetMapping(value = "/sendMessage")
+    public String sendMessage()
+    {
+        return messageProvider.send();
+    }
+}
+
---
+++ springcloud-stream-rabbitmq-provider8801/src/main/java/com/wangxg/springcloud/service/IMessageProvider.java
@@ -0,0 +1,6 @@
+package com.wangxg.springcloud.service;
+
+public interface IMessageProvider
+{
+    public String send() ;
+}
