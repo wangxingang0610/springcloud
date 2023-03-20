75e57a1231ad4b92ceb06469536ba07f761c60e2
wangxingang <wangxingang0610@sina.com>
2023-03-20 16:34:28 +0800

    nacos

---
+++ springcloudalibaba-consumer-nacos-order83/src/main/java/com/wangxg/springcloud/controller/PaymentController.java
@@ -0,0 +1,28 @@
+package com.wangxg.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+public class PaymentController
+{
+    @Value("${service-url.nacos-user-service}")
+    private String serverUrl;
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping(value = "/consumer/payment/{id}")
+    public String getPayment(@PathVariable("id") Integer id)
+    {
+        return restTemplate.getForObject(serverUrl + "/payment/nacos/" + id, String.class);
+    }
+}
+
+
+
